43caacd61dfd402846541b09f8f66b4ef3503768
thinksysuser <pandeyrajat333@gmail.com>
2020-10-23 18:59:01 +0530

    created table database

--- controller/database.js
+++ controller/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-module.exports = new Sequelize({
+const database = new Sequelize({
     dialect: "postgres",
     host: "localhost",
     port: 5433,
@@ -8,6 +8,37 @@ module.exports = new Sequelize({
     database: "movies",
 });
 
+const dbTable = sequelize.define(
+    "nodeDB",// database name
+    {
+    id:{
+    type:DataTypes.NUMBER ,
+    allowNull:false,
+    primaryKey:true,
+    },
+    name: {
+    type: DataTypes.STRING,
+    },
+    
+    },
+    { createdAt: false, updatedAt: false }
+    );
+    
+    const init = async function () {
+    try {
+    await sequelize.authenticate();
+    await sequelize.sync({alter:true})
+    // const data = await MasterKey.findAll({});
+    
+    // console.log("db > init > data", data);
+    } catch (error) {
+    // console.log("db > init > ", error);
+    }
+    };
 
 
+module.exports{
+    database,
+    table,
 
+}
--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const db= require("./controller/database")
 const app = express();
 
 
-db.authenticate()
-.then(()=>console.log("database connect"))
-.catch((error)=>console.log(`${error} error`))
+//middleware
+app.use();
+app.use();
+
+//controller
+app.get()
+
+app.post()
+
+app.put()
+
+app.delete()
+
+//database_connection
+
 
 
 app.listen(3000, console.log("running on the port 3000"));
